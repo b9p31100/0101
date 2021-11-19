fd934f63a0456c7c5f3a993d76861ce42570f506
aaaaidiot <b9p31081@bunkyo.ac.jp>
2021-11-19 14:57:22 +0900

    gradleの修正、クラスの追加

---
+++ app/src/main/java/com/example/a0101/userregister.java
@@ -0,0 +1,14 @@
+package com.example.a0101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userregister extends AppCompatActivity {
+    //ユーザー登録画面
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userregister);
+    }
+}
