790ff790309e7b2b920b7eeb49dcc89b842ca94f
chisato <b9p31044@shonan.bunkyo.ac.jp>
2021-11-20 22:50:29 +0900

    編集しました

---
+++ app/src/main/java/com/example/a0101/hanteikextuka.java
@@ -0,0 +1,14 @@
+package com.example.a0101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hanteikextuka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hanteikextuka);
+    }
+}
