8e437c7af245c80f9b4702c00e84cbdf782b5e6a
b9p31100 <b9p31100@shonan.bunkyo.ac.jp>
2021-12-10 00:30:12 +0900

    OCRyari

---
+++ app/src/main/java/com/example/a0101/qcrkeka.java
@@ -0,0 +1,14 @@
+package com.example.a0101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class qcrkeka extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qcrkeka);
+    }
+}
