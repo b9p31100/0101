bdeb525c838f237becd0ed0bfc592b858c4b3f6c
chisato <b9p31044@shonan.bunkyo.ac.jp>
2021-12-10 13:07:50 +0900

    編集ok

--- app/src/main/java/com/example/a0101/hanteikextuka.java
+++ app/src/main/java/com/example/a0101/hanteikextuka.java
@@ -3,17 +3,25 @@ package com.example.a0101;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class hanteikextuka extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hanteikextuka);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-        Animation animation = AnimationUtils.loadAnimation(MainActivity.this,R.anim.class)
-
+            ImageView imageView = (ImageView) findViewById(R.id.imageView14);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Animation animation = AnimationUtils.loadAnimation(hanteikextuka.this, R.anim.btn_action);
+                    view.startAnimation(animation);
+                }
+            });
+        }
     }
-}
