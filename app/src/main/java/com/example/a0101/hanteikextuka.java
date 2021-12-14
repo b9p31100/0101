48e5e5cc5da2cb17a9f2f82f4d83341c0bb6c423
chisato <b9p31044@shonan.bunkyo.ac.jp>
2021-12-15 01:13:34 +0900

    design ok

--- app/src/main/java/com/example/a0101/hanteikextuka.java
+++ app/src/main/java/com/example/a0101/hanteikextuka.java
@@ -1,6 +1,8 @@
 package com.example.a0101;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,25 @@ public class hanteikextuka extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_hanteikextuka);
+            ImageButton btn1 = (ImageButton) findViewById(R.id.imageButton1);
+            ImageButton btn2 = (ImageButton) findViewById(R.id.imageButton3);
+            ImageButton btn3 = (ImageButton) findViewById(R.id.imageButton2);
+            ImageButton btn4 = (ImageButton) findViewById(R.id.imageButton4);
+
+            btn1.setOnClickListener(v -> {
+                startActivity(new Intent(hanteikextuka.this, home.class));
+            });
+            btn2.setOnClickListener(v -> {
+                startActivity(new Intent(hanteikextuka.this, option.class));
+            });
+            btn3.setOnClickListener(v -> {
+                startActivity(new Intent(hanteikextuka.this, setting.class));
+            });
+            btn4.setOnClickListener(v -> {
+                startActivity(new Intent(hanteikextuka.this, profiletregister.class));
+            });
+        }
+
 
         }
-    }
+
--- app/src/main/java/com/example/a0101/home.java
+++ app/src/main/java/com/example/a0101/home.java
@@ -18,38 +18,43 @@ public class home extends AppCompatActivity {
         setContentView(R.layout.activity_home);  //activity_homeを遷移先のページの名前に変える(xml)
         //変数はこのファイルだけなので、「btn」と置いても、別のjavaファイルで「btn」でも問題ないです
         //Button btn = (Button) findViewById(R.id.return_button);//return_buttonのところをボタンのid名に変える
-        Button btn2 =(Button) findViewById(R.id.button_okini);
-        Button btn3 =(Button)findViewById(R.id.button_puro);
-        Button btn4 =(Button)findViewById(R.id.button_ocr);
-        Button btn5 =(Button)findViewById(R.id.button2);
-        Button btn6 =(Button)findViewById(R.id.button_bako);
-        ImageButton btn7 =(ImageButton) findViewById(R.id.imageButton3);
+        Button btn2 = (Button) findViewById(R.id.button_okini);
+        Button btn3 = (Button) findViewById(R.id.button_puro);
+        Button btn4 = (Button) findViewById(R.id.button_ocr);
+        Button btn6 = (Button) findViewById(R.id.button_bako);
+        ImageButton btn7 = (ImageButton) findViewById(R.id.imageButton3);
+        ImageButton btn8 = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton btn9 = (ImageButton) findViewById(R.id.imageButton4);
         mAuth = FirebaseAuth.getInstance();
 
         //ラムダ式の方が簡単に書けます
 
-        btn2.setOnClickListener(v ->{
+        btn2.setOnClickListener(v -> {
             startActivity(new Intent(home.this, //homeを遷移先のページの名前に変える(java)
-                    okiniri.class) );
+                    okiniri.class));
             //お気に入り画面に遷移
         });
-        btn3.setOnClickListener(v ->{
+        btn3.setOnClickListener(v -> {
             startActivity(new Intent(home.this, //homeを遷移先のページの名前に変える(java)
-                    profiletregister.class) );
+                    profiletregister.class));
             //ユーザー登録に遷移
         });
         //OCRに遷移
-        btn4.setOnClickListener(v ->{
+        btn4.setOnClickListener(v -> {
             startActivity(new Intent(home.this, post.class));
         });
-        btn5.setOnClickListener(v ->{
-            startActivity(new Intent(home.this, hanteikextuka.class));
-        });
-        btn6.setOnClickListener(v ->{
+
+        btn6.setOnClickListener(v -> {
             startActivity(new Intent(home.this, barcode.class));
         });
-        btn7.setOnClickListener(v ->{
+        btn7.setOnClickListener(v -> {
             startActivity(new Intent(home.this, option.class));
+        });
+        btn8.setOnClickListener(v -> {
+            startActivity(new Intent(home.this, setting.class));
+        });
+        btn9.setOnClickListener(v -> {
+            startActivity(new Intent(home.this, profiletregister.class));
         });
     }
 }
